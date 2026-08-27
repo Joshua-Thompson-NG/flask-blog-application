584d6a3d244594b97692d808115e6dc6ca4c2793
Thompson <o.thompson@alustudent.com>
2026-08-27 15:55:30 +0000

    Added sqlite database

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os
 
 from flask import Flask,render_template,url_for,flash,redirect
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 from forms import RegistrationForm,LoginForm
+
+from models import User,Post
 
 # Load environment variables from .env file
 load_dotenv()
 
-app = Flask(__name__)
-
+# --- Flask Setup/Configuration ---
+app = Flask(__name__) # flask instance
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app) # db instance
 
+
+# --- Dummy Data ---
 posts = [
     {
         'author':'Corey Schafer',
@@ -33,6 +40,8 @@ posts = [
         'date_posted':'July 22, 2020'
     }
 ]
+
+# --- Page Routes ---
 @app.route('/')
 @app.route('/home')
 def home():
@@ -41,6 +50,10 @@ def home():
 @app.route('/about')
 def about():
     return render_template("about.html",title="About")
+
+@app.route('/contact')
+def contact():
+    return "<h1> Contact Page </h1>"
 
 
 # --- Form Routes ---
@@ -59,5 +72,8 @@ def login():
     return render_template('login.html',title='Login',form=form)
 
 
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
