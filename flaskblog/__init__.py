f87d82c4e010c747812d45331e540ed85bf1851b
Thompson <o.thompson@alustudent.com>
2026-08-27 20:20:10 +0000

    Added userlogin and logout along with account page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,14 +3,23 @@ import os
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # Load environment variables from .env file
 load_dotenv()
 
 # --- Flask Setup/Configuration ---
 app = Flask(__name__) # flask instance
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY',"dev-secret-key")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) # db instance
+bcrypt = Bcrypt(app)  # bcrypt instance
+
+
+login_manager = LoginManager(app) # login manager instance
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired,Length,Email,EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
+
 
 # Registration Form
 class RegistrationForm(FlaskForm):
@@ -13,6 +15,22 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password')])
 
     submit = SubmitField('Sign Up')
+
+    # Validation template
+    # def validate_field(self,field):
+    #     if field.data == '':
+    #         raise ValidationError('This field is required.')
+
+    # Validation Checks
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('The username is already taken. Please choose a different one.')
+
+    def validate_email(self,email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('The email is already taken. Please choose a different one.')
 
 
 class LoginForm(FlaskForm):
